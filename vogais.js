8f50149f524314dea2de08312c7c783fc56920f0
Giovanni <giovanni.ggoncalves@outlook.com>
2024-03-20 16:57:48 -0300

    contagem de consoantes e vogais

--- vogais.js
+++ vogais.js
@@ -1,21 +1,27 @@
 let vogais = ["a", "e", "i", "o", "u"];
-let consoantes = ["b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
-let frase = "giovanni na parada"; 
-frase = frase.toLowerCase();
+let consoantes = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+let frase = "Giovanni na parada";
+frase = frase.toLowerCase()
 let letras = frase.split("");
-let letrasVogais = [];
-let letrasConsoantes = [];
-for(let i = 0; i < letras.length; i++){
-    if (vogais.includes(letras[i])) {
-        letrasVogais.push(letras[i]);
-        
-     } else (consoantes.includes(letras[i])) {
-        letrasConsoantes.push(letras[i]);
+let totalVogais = [];
+let totalConsoantes = [];
+let vazio = [" "];
+let espaco = [];
+for(i = 0; i < letras.length; i++){
+    if(vogais.includes(letras[i])){
+        totalVogais.push(letras[i])
+    }else 
+    if(consoantes.includes(letras[i])){
+        totalConsoantes.push(letras[i])
+    }else
+    if(vazio.includes(letras[i])){
+    espaco.push(letras[i])
     }
 }
-console.log("texto original:", frase);
-console.log("vogais encontradas:", letrasVogais.toString());
-console.log("total de vogais:" + letrasVogais.length);
-console.log("total de consoantes:" + letrasConsoantes.length);
-console.log("consoantes encontradas:", letrasConsoantes.toString());
-console.log("total de espaços:" + totalEspacos.lenght);
+
+console.log("frase original:" + frase)
+console.log("o total de vogais é:" + totalVogais.length)
+console.log ("vogais encontradas:" + totalVogais.toString())
+console.log("total de consoantes é:" + totalConsoantes.length)
+console.log("consoantes encontradas:" + totalConsoantes.toString())
+console.log("total de espaços:" + espaco.length)
